0b937ce494b3b29491c181775c5d5901d2756b88
hyeonseokhan <toycode727@gmail.com>
2024-07-30 09:25:41 +0900

    :sparkles: 소개 정보 업데이트

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -1,13 +1,17 @@
 import { IIntroduce } from '../components/introduce/IIntroduce';
-import { lastestUpdatedAt } from '../package.json';
+import { lastestUpdatedAt, author } from '../package.json';
 
 const introduce: IIntroduce.Payload = {
   disable: false,
   contents: [
-    'As the cherished emblem of the Linux kernel, I have become synonymous with open-source philosophy and the free software movement worldwide. My career began in the icebergs of development, where the seeds of my fascination with operating systems first sprouted. From a mere hatchling in the vast sea of code, I quickly ascended to become a pivotal figure in the Linux community. My journey has been marked by relentless dedication to system integrity, scalability, and robustness. Under my vigilant watch, countless servers have achieved unparalleled uptimes, and systems have been optimized to run like the well-oiled machines they are intended to be.',
-    'In the world of zeros and ones, my expertise has been instrumental in pioneering advancements in Linux system engineering. My flippers have crafted meticulous kernel enhancements, each aimed at streamlining performance and bolstering security across distributed networks. I am known in the community for my ability to dive deep into complex codebases and emerge with pearls of innovation that drive the collective progress of Linux environments. Advocacy is also a part of my plumage – spreading the word about the virtues of open-source and encouraging the adoption of Linux, from personal desktops to corporate data centers. As a mentor, I have nurtured the growth of young hatchlings into seasoned professionals, contributing to the diversity and vitality of our ecosystem.',
+    '공개 키 기반 구조(Public Key Infrastructure, PKI) 보안 인증솔루션 기술력을 바탕으로 탈중앙화 신원 증명(Decentralized Identity, DID)과 블록체인을 활용한 응용개발의 경험이 있습니다.',
+    '주로 스프링 프레임워크를 이용한 RESTful API 백엔드 개발을 담당하였으나 업무의 영역을 가리지 않고 프로젝트 진행에 있어 발생하는 문제를 고민하고 해결하는 것을 좋아합니다.',
+    '최근에는 허가형 블록체인 플랫폼인 Hyperledger Fabric의 성능 개선 및 실사용 측면에 관심이 있습니다.',
+    'I am an experienced application developer using decentralized identity (DID) and blockchain based on the technology of the Public Key Infrastructure (PKI) security authentication solution.',
+    'I was mainly in charge of RESTful API backend development using the Java spring framework. Furthermore, I like to focus on the process of considering and solving problems that occurs during any of project progresses.',
+    'In addition, I recently got interested in the performance improvement and practical use of Hyperledger Fabric a permission blockchain platform.',
   ],
-  sign: 'Tux',
+  sign: author.name,
   latestUpdated: lastestUpdatedAt,
 };
 
